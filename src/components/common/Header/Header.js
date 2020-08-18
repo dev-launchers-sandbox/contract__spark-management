659e97e7cf2e011e37b36c36f4130fc833291d7b
Alejandro <alearm246@gmail.com>
2020-08-18 13:42:26 -0700

    took off create user off header and replaced it with a button, added an emoji to all the buttons

--- src/components/common/Header/Header.js
+++ src/components/common/Header/Header.js
@@ -31,7 +31,6 @@ function Header() {
       {/*}Actual visual body: {*/}
       <img className={style.sparkLogo} src={SparkLogo} alt="sparkLogo" />
       <h1>{username.toUpperCase()}</h1>
-      <a style={{textDecoration: "none", fontWeight: "bold"}}><Link to="/CreateNewUser" style={{textDecoration: "none"}}>👤 Add New User</Link></a>
       <a style={{textDecoration: "none", fontWeight: "bold"}}onClick={logout}>🚪 Log Out</a>
     </div>
   );
