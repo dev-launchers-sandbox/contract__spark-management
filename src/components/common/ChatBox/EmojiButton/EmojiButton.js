29b52ed386592c9dd7406a20989f1e9712095fa5
Alejandro <alearm246@gmail.com>
2020-10-31 10:52:22 -0700

    added emoji button, added hover functionality, and added emoji picker

---
+++ src/components/common/ChatBox/EmojiButton/EmojiButton.js
@@ -0,0 +1,30 @@
+import React, {useEffect, useState} from "react";
+import { Picker } from 'emoji-mart'
+import "emoji-mart/css/emoji-mart.css";
+
+import style from "./EmojiButton.module.css";
+
+function EmojiButton(){
+
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [currentEmoji, setCurrentEmoji] = useState(null);
+
+  const handleClick = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  }
+
+  const handleEmojiClick = (emoji) => {
+    setCurrentEmoji(emoji.native);
+    console.log(emoji.native);
+  }
+
+  return(
+    <div className={style.emojiButtonContainer}>
+      <button className={style.emojiButton} onClick={handleClick}>😀+</button>
+      {showEmojiPicker && <Picker title="pick your emoji" onSelect={handleEmojiClick} theme="dark"/>}
+    </div>
+  )
+}
+
+
+export default EmojiButton;
--- src/components/common/ChatBox/Message/Message.js
+++ src/components/common/ChatBox/Message/Message.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
 import style from "./Message.module.css";
+import EmojiButton from "../EmojiButton/EmojiButton.js";
 
 function Message(props) {
+
+  const [showButton, setShowButton] = useState(false);
   /*
   const imageExists = (image_url) => {
     try {
@@ -19,15 +22,24 @@ function Message(props) {
   */
 
   return (
-    <div className={style.messageHolder}>
+    <div
+      className={props.message.server ? style.messageHolderServer : style.messageHolder}
+      onMouseOver={props.message.server ? null : () => setShowButton(true)}
+      onMouseLeave={props.message.server ? null : () => setShowButton(false)}
+    >
       {!props.message.server && (
         <div className={style.author}>
           <b>{props.message.author} </b>{" "}
           <div className={style.date}> {props.message.timestamp} </div>
+          <div className={style.emojiButton}>
+            {showButton && <EmojiButton />}
+          </div>
         </div>
       )}
 
-      <div className={props.message.server ? style.server : style.content}>
+      <div
+        className={props.message.server ? style.server : style.content}
+      >
         {props.message.content}
       </div>
     </div>
