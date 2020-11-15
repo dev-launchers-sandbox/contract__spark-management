63e7b87aa5b989a1507981e101252e5f99f49a3f
Alejandro <alearm246@gmail.com>
2020-11-14 21:33:47 -0800

    replaced room code with the sub_client

--- src/components/common/ChatBox/ChatHeader/ChatHeader.js
+++ src/components/common/ChatBox/ChatHeader/ChatHeader.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import style from "./ChatHeader.module.css";
+import axios from "axios";
 
 function ChatHeader(props) {
+  const [subClient, setSubClient] = useState("");
+
+
+  useEffect(() => {
+    const getSubClient = async () => {
+      const res = await axios.get(`https://api.spark4community.com/codes/${props.room()}`);
+      console.log(res)
+      const subClient = res.data.code.sub_client_name === "" ? res.data.code.client_name : res.data.code.sub_client_name;
+      setSubClient(subClient);
+    }
+    getSubClient();
+  }, [])
+
+
   return (
     <div className={style.chatHeader}>
-      <p className={style.room}> Room: {props.room()} </p>
+      <p className={style.room}> {subClient} </p>
       <span className={style.closeToggle} onClick={props.handleClose}>
         ✖️
       </span>
