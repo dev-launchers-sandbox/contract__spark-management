56b8e709ad102d4812d20a86b2a27789faf4173e
Alejandro <alearm246@gmail.com>
2020-12-01 19:10:32 -0800

    added titles to the red deck and the icons. also made the icons darker.

--- src/components/common/Icons/SelectCardIcon/SelectCardIcon.js
+++ src/components/common/Icons/SelectCardIcon/SelectCardIcon.js
@@ -11,6 +11,7 @@ export default function SelectCardIcon(props) {
         src={drawCardIcon}
         alt="Select Card"
         onClick={props.onClick}
+        title="draw new card"
       />
     </div>
   );
