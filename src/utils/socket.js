917b0001fcda31b39584350cda3744b8fdc5f441
alearm <alearm246@gmail.com>
2020-12-21 18:46:06 -0600

    refactroing

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io("https://api.spark4community.com:8000");
+//TODO change ip to domain address
+
+const socket = io("http://localhost:8080");
 
 export default socket;
