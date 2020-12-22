9130a4cd9d3b8e4a49ef5daff44dbea4f92fd8a2
Guillermo S <gsantaolalla2006@gmail.com>
2020-12-21 18:12:22 -0600

    Changed the sockets url to the IP address.

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,5 +1,6 @@
 import io from "socket.io-client";
 
-const socket = io("https://api.spark4community.com:8000");
+//TODO: Change the IP address to the spark API url: https://api.spark4community.com:8000
+const socket = io("http://192.232.212.61:8000");
 
 export default socket;
